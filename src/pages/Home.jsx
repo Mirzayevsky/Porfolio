c57f8d5232dc9a18b395d9c19a47e66d82e33678
sirojiddin <mirzayevsky@gmail.com>
2023-11-08 10:37:45 +0500

    styles changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
-
+import "./home.scss"
 const Home = () => {
     return (
-            <div>
+            <div className='container'>
                 <h1>Minimalism</h1>
                 <h1>Creativity</h1>
                 <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+                <h1>Simplicity</h1>
+
+
             </div>
     )
 }
