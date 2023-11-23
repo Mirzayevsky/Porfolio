5d18081efbe89e33528181c3a75e28eb3737778f
sirojiddin <mirzayevsky@gmail.com>
2023-11-23 11:03:22 +0500

    folders are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
     return (
         <div>
             hello
         </div>
-        // <BrowserRouter>
-        //     <Routes>
-        //         <Route path='/' element={<MainLayout />}>
-        //             <Route index element={<Home />}/>
-        //             <Route path='service' element={<Service/>}/>
-        //             <Route path='contact' element={<Contact/>} />
-        //         </Route>
-        //     </Routes>
-        // </BrowserRouter>
+      
     )
 }
 
